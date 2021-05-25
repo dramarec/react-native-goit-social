fe9cec850fc78059acb4d10aeffcef6e52cb0abd
Oleg Dramaretskyi <dramarec@gmail.com>
2021-05-25 16:57:41 +0300

    added form

---
+++ App.js
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    ImageBackground,
+    TextInput,
+    TouchableOpacity,
+    Platform,
+    Keyboard,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback
+} from "react-native";
+
+const initialState = {
+    email: "",
+    password: "",
+};
+
+export default function App() {
+    // console.log("{*} ===> App ===> Platform.OS", Platform.OS);
+    const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+    const keyboardHide = () => {
+        setIsShowKeyboard(false);
+        Keyboard.dismiss();
+    };
+
+    return (
+        <TouchableWithoutFeedback onPress={keyboardHide} >
+
+            <View style={styles.container}>
+                <ImageBackground
+                    style={styles.image}
+                    source={require("./assets/images/stars-on-night.jpg")}
+                >
+
+                    <KeyboardAvoidingView
+                        behavior={Platform.OS == "ios" ? "padding" : "height"}
+                    >
+
+                        <View /* style={styles.form} */
+                            style={{ ...styles.form, marginBottom: isShowKeyboard ? 20 : 150 }}
+                        >
+                            <View style={styles.header}>
+                                <Text style={styles.headerTitle}>Hello again</Text>
+                                <Text style={styles.headerTitle}>Welcome back</Text>
+                            </View>
+                            <View>
+                                <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    textAlign={"center"}
+                                    onFocus={() => setIsShowKeyboard(true)}
+                                />
+                            </View>
+
+                            <View style={{ marginTop: 20 }}>
+                                <Text style={styles.inputTitle}>PASSWORD</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    textAlign={"center"}
+                                    secureTextEntry={true}
+                                    onFocus={() => setIsShowKeyboard(true)}
+                                />
+                            </View>
+
+                            {/* отличаеться на адроид */}
+                            {/* <Button title="SIGN IN" /> */}
+                            <TouchableOpacity
+                                activeOpacity={0.8}
+                                style={styles.btn}
+                                onPress={keyboardHide}
+
+                            >
+                                <Text style={styles.btnTitle}>SIGN IN</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </KeyboardAvoidingView>
+
+                </ImageBackground>
+            </View>
+        </ TouchableWithoutFeedback>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        // justifyContent: "center",
+        justifyContent: "flex-end",
+        // alignItems: "center",
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#f0f8ff",
+        height: 40,
+        borderRadius: 6,
+        color: "#f0f8ff",
+    },
+    form: {
+        marginHorizontal: 40,
+    },
+    inputTitle: {
+        color: "#f0f8ff",
+        marginBottom: 10,
+        fontSize: 18,
+    },
+    btn: {
+        height: 40,
+        borderRadius: 6,
+        borderWidth: 1,
+        marginTop: 40,
+        justifyContent: "center",
+        alignItems: "center",
+        marginHorizontal: 20,
+        //? border: Platform.OS === 'ios' ? 1 : 6,
+        ...Platform.select({
+            ios: {
+                backgroundColor: "transparent",
+                borderColor: "#f0f8ff",
+            },
+            android: {
+                backgroundColor: "#4169e1",
+                borderColor: "transparent",
+            },
+        }),
+    },
+    btnTitle: {
+        // color: "#f0f8ff",
+        color: Platform.OS === "ios" ? "#4169e1" : "#f0f8ff",
+
+        fontSize: 18,
+    },
+    header: {
+        alignItems: "center",
+        marginBottom: 150,
+    },
+    headerTitle: {
+        fontSize: 30,
+        color: "#f0f8ff",
+    },
+});
