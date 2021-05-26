4d2d41c37df476a49febc46918b0df720ebb5db9
Oleg Dramaretskyi <dramarec@gmail.com>
2021-05-26 14:11:48 +0300

    refuctoring route and MainTab tabBarIcon

--- App.js
+++ App.js
@@ -1,49 +1,12 @@
 import React from "react";
 import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import RegisterScreen from "./screens/auth/RegisterScreen";
-import LoginScreen from "./screens/auth/LoginScreen";
-import PostsScreen from "./screens/mainScreen/PostsScreen";
-import CreateScreen from "./screens/mainScreen/CreateScreen";
-import ProfileScreen from "./screens/mainScreen/ProfileScreen";
-
-const AuthStack = createStackNavigator();
-const MainTab = createBottomTabNavigator()
-
-const useRoute = (isAuth) => {
-    if (!isAuth) {
-        return (
-            <AuthStack.Navigator>
-
-                <AuthStack.Screen
-                    options={{ headerShown: false }}
-                    name='Register'
-                    component={RegisterScreen}
-                />
-
-                <AuthStack.Screen
-                    options={{ headerShown: false }}
-                    name='Login'
-                    component={LoginScreen}
-                />
-
-            </AuthStack.Navigator>
-        );
-    }
-    return (
-        <MainTab.Navigator>
-            <MainTab.Screen name="Posts" component={PostsScreen} />
-            <MainTab.Screen name="Create" component={CreateScreen} />
-            <MainTab.Screen name="Profile" component={ProfileScreen} />
-        </MainTab.Navigator>
-    );
-};
+import { useRoute } from "./router";
 
 export default function App() {
-    const routing = useRoute(null)
+    // null => auth, {} => MainTab
+    const routing = useRoute({})
     const [loaded] = useFonts({
         "DMMono-Regular": require("./assets/fonts/DMMono-Regular.ttf"),
     });
--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ```sh
 -   yarn add expo-font
+-   yarn add @react-navigation/native
+-   yarn add react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+-   yarn add @react-navigation/stack
+-   yarn add @react-navigation/bottom-tabs
 ```
