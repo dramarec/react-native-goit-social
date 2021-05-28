7927b9bc243f85dd4ce597b27f2a5bf261753e95
Oleg Dramaretskyi <dramarec@gmail.com>
2021-05-28 18:34:15 +0300

    CommentsScreen getAllPosts

--- screens/nestedScreens/CommentsScreen.js
+++ screens/nestedScreens/CommentsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import {
     View,
@@ -6,6 +6,8 @@ import {
     StyleSheet,
     TextInput,
     TouchableOpacity,
+    SafeAreaView,
+    FlatList,
 } from "react-native";
 import db from "../../firebase/config";
 
@@ -13,8 +15,13 @@ const CommentsScreen = ({ route }) => {
     const { postId } = route.params;
 
     const [comment, setComment] = useState("");
+    const [allComments, setAllComments] = useState([]);
 
     const { nickName } = useSelector(state => state.auth);
+
+    useEffect(() => {
+        getAllPosts();
+    }, []);
 
     const createPost = async () => {
         db.firestore()
@@ -24,16 +31,45 @@ const CommentsScreen = ({ route }) => {
             .add({ comment, nickName });
     };
 
+    const getAllPosts = async () => {
+        db.firestore()
+            .collection("posts")
+            .doc(postId)
+            .collection("comments")
+            .onSnapshot(data =>
+                setAllComments(data.docs.map(doc =>
+                    ({ ...doc.data(), id: doc.id })
+                ))
+            );
+    };
+
     return (
         <View style={styles.container}>
-            <Text>CommentsScreen</Text>
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={allComments}
+                    renderItem={({ item }) => (
+                        <View style={styles.commentContainer}>
+                            <Text>{item.nickName}</Text>
+                            <Text>{item.comment}</Text>
+                        </View>
+                    )}
+                    keyExtractor={(item) => item.id}
+                />
+            </SafeAreaView>
+
 
             <View style={styles.inputContainer}>
-                <TextInput style={styles.input} onChangeText={setComment} />
+                <TextInput style={styles.input}
+                    onChangeText={setComment} />
             </View>
-            <TouchableOpacity onPress={createPost} style={styles.sendBtn}>
+
+            <TouchableOpacity style={styles.sendBtn}
+                onPress={createPost}
+            >
                 <Text style={styles.sendLabel}>add post</Text>
             </TouchableOpacity>
+
         </View>
     )
 };
@@ -42,6 +78,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "flex-end",
+    },
+    commentContainer: {
+        borderWidth: 1,
+        borderColor: "#20b2aa",
+        marginHorizontal: 10,
+        padding: 10,
+        marginBottom: 10,
     },
     sendBtn: {
         marginHorizontal: 30,
